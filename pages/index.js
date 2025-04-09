1be648bcb3224c1b9809c87ab9eb7d380786ce41
mlmcnally <mlou@mycontentlaboratory.com>
2025-04-09 15:04:35 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>The Reputation Bank App</h1>
+      <p>Interactive workbook coming soon...</p>
+    </div>
+  );
+}
