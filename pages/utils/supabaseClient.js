f38e219ad5144caa19a036fdb408173127644476
mlmcnally <mlou@mycontentlaboratory.com>
2025-04-14 14:30:33 -0700

    Update supabaseClient.js

--- pages/utils/supabaseClient.js
+++ pages/utils/supabaseClient.js
@@ -1,10 +1,6 @@
-// utils/supabaseClient.js
-
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-export { supabase }
