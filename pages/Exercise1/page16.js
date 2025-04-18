ee876d6abf04064ccbc6ea0fda535970037a95a6
mlmcnally <mlou@mycontentlaboratory.com>
2025-04-18 13:58:54 -0700

    Update page16.js

--- pages/Exercise1/page16.js
+++ pages/Exercise1/page16.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { supabase } from '../utils/supabaseClient'; 
+import { supabase } from '../../utils/supabaseClient'; 
 
 export default function Exercise1Page16() {
   const [response, setResponse] = useState('')
