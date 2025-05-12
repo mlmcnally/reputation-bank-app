f5e0ab86551550b60c5c42d2f4c410afec555bfb
mlmcnally <mlou@mycontentlaboratory.com>
2025-05-12 14:22:24 -0700

    Correct supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -3,7 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL // URL of Supabase project
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // Anon key from Supabase
 
-// Create the Supabase client using these variables
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export { supabase }
