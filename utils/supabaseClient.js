048ca69863c40ea76f84aa6239d0ae637ad88c4b
mlmcnally <mlou@mycontentlaboratory.com>
2025-05-12 13:12:00 -0700

    Update supabaseClient.js

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,8 +1,7 @@
-import { supabase } from '../../utils/supabaseClient';
+import { createClient } from '@supabase/supabase-js'
 
-// Get environment variables for Supabase URL and anon key
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL // URL of Supabase project
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // Anon key from Supabase
 
 // Create the Supabase client using these variables
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
