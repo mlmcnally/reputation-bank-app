4900ccce5e7961c5d353aab2da3d00f65c9fb7a9
mlmcnally <mlou@mycontentlaboratory.com>
2025-06-03 12:45:10 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ import Flipbook from '../components/Flipbook';
 
 export default function Home() {
   return (
-    <main style={{ padding: '1rem' }}>
-      <h1 style={{ textAlign: 'center' }}>The Reputation Bank Workbook</h1>
+    <main style={{ padding: '1rem' }}
       <Flipbook />
     </main>
   );
