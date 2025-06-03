38555d393394d3bbc02b79657db96d7534e89368
mlmcnally <mlou@mycontentlaboratory.com>
2025-06-03 12:50:54 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Flipbook from '../components/Flipbook';
 
 export default function Home() {
   return (
-    <main style={{ padding: '1rem' }}
+    <main style={{ padding: '1rem' }}>
       <Flipbook />
     </main>
   );
