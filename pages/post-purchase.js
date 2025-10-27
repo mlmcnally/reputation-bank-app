b7ed75d6577904c038930d57fadbd77a599882a9
mlmcnally <mlou@mycontentlaboratory.com>
2025-10-27 16:47:22 -0700

    Create post-purchase.js

---
+++ pages/post-purchase.js
@@ -0,0 +1,9 @@
+export default function PostPurchase() {
+  return (
+    <main style={{maxWidth: 600, margin: '2rem auto', fontFamily: 'Avenir, "Nunito Sans", sans-serif'}}>
+      <h2>Thanks for your purchase!</h2>
+      <p>Your account is being activated. This usually takes a few seconds.</p>
+      <a href="/" style={{color:'#084a58'}}>Go to the app</a>
+    </main>
+  );
+}
